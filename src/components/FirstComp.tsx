7adbd9c3e6ea959b2c00732de63cb50a0d44da11
lorraine98 <limgytjd98@gmail.com>
2022-12-25 18:22:00 +0900

    make a feature

--- src/components/FirstComp.tsx
+++ src/components/FirstComp.tsx
@@ -2,6 +2,7 @@ export const FirstComp = () => {
   return (
     <div style={{ backgroundColor: "red", height: "100%" }}>
       <h2>First Component</h2>
+      <div>my new feature a</div>
     </div>
   );
 };
