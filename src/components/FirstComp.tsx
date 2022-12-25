1fe95c5aa9790b223a3288d5cedb9085871c520d
lorraine98 <limgytjd98@gmail.com>
2022-12-25 18:10:42 +0900

    Add a to first comp

--- src/components/FirstComp.tsx
+++ src/components/FirstComp.tsx
@@ -2,6 +2,9 @@ export const FirstComp = () => {
   return (
     <div style={{ backgroundColor: "red", height: "100%" }}>
       <h2>First Component</h2>
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
     </div>
   );
 };
